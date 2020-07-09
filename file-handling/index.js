e69d30d30cbe08612b7f6f7ad746e3b95c94edc9
Helio Frota <00hf11@gmail.com>
2020-07-09 17:25:45 -0300

    enabling the test

--- file-handling/index.js
+++ file-handling/index.js
@@ -8,20 +8,18 @@ const options = {};
 
 const breaker = new CircuitBreaker(readFile, options);
 
-
 function fire() {
-  // breaker.fire('./package.json', 'utf-8')
-  // .then()
-  // .catch(error => console.error(error));
+  breaker.fire('./package.json', 'utf-8')
+  .then()
+  .catch(error => console.error(error));
   
-  // // breaker.on('success', () => console.log(1));
-  // // breaker.on('timeout', () => console.log(2));
+  // breaker.on('success', () => console.log(1));
+  // breaker.on('timeout', () => console.log(2));
   // breaker.on('reject', () => console.log(3));
   // breaker.on('open', () => console.log(4));
   // breaker.on('halfOpen', () => console.log(5));
   // breaker.on('close', () => console.log(6));
   // breaker.on('fallback', () => console.log(7));
-  'a' + JSON.stringify('b');
 }
 
 module.exports = {
